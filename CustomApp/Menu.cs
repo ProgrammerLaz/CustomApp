28c56b40014e923de4e428d242af61759342ddb3
Lazaro Rondon <LARondon@student.fullsail.edu>
2020-05-24 20:18:58 -0400

    Add project files.

---
+++ CustomApp/Menu.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace CustomApp
+{
+    class Menu
+    {
+        public static void NewNote()
+        {
+            string option;
+            do
+            {
+                Console.WriteLine("What would you like to add to the note? (maximum of 254 characters)");
+                option = Console.ReadLine();
+            }
+            while (string.IsNullOrWhiteSpace(option) || option.Length > 254);
+            string option2;
+            DateTime date;
+            do
+            {
+                Console.WriteLine("When was this note created? (date format only)");
+                option2 = Console.ReadLine();
+            }
+            while (string.IsNullOrWhiteSpace(option2) || !DateTime.TryParse(option2, out date));
+            //Initiates a MySQL connection and executes a MySQL command to insert data into a table
+            _ = new Database(Program.cs);
+            MySqlCommand comm = Database.con.CreateCommand();
+            comm.CommandText = "INSERT INTO totalNotes(userID,noteDate,noteText) VALUES(@userID, @noteDate, @noteText)";
+            comm.Parameters.AddWithValue("@noteDate", date);
+            comm.Parameters.AddWithValue("@noteText", option);
+            comm.Parameters.AddWithValue("@userID", 1);
+            comm.ExecuteNonQuery();
+            Database.con.Close();
+            Console.WriteLine("The note was successfully added to the database!");
+        }
+        public static void ReadNote()
+        {
+            string option;
+            DateTime date;
+            do
+            {
+                Console.WriteLine("When was this note created? (date format only)");
+                option = Console.ReadLine();
+            }
+            while (string.IsNullOrWhiteSpace(option) || !DateTime.TryParse(option, out date));
+            //Initiates a MySQL connection and executes a MySQL command to search for columns in a table
+            DataTable dt = new DataTable();
+            _ = new Database(Program.cs);
+            using (MySqlCommand cmd = new MySqlCommand("select * from totalNotes where noteDate = @notedate", Database.con))
+            {
+                cmd.Parameters.AddWithValue("@notedate", date);
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["noteID"].ToString() + " " + reader["noteText"].ToString() + " " + reader["noteDate"].ToString());
+                }
+                Database.con.Close();
+            }
+        }
+    }
+}
---
+++ CustomApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CustomApp
+{
+    class Program
+    {
+        //Database Location
+        public static string cs = @"server= 127.0.0.1;userid=root;password=root;database=notefinity;port=3306";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Notefinity, what would you like to do today?");
+            Console.WriteLine("1. New Note");
+            Console.WriteLine("2. Read Note");
+            Console.WriteLine("3. Exit");
+            string option = Console.ReadLine();
+            //Loop the many options in the given list for the user
+            switch (option)
+            {
+                case "1":
+                    Menu.NewNote();
+                    break;
+                case "2":
+                    Menu.ReadNote();
+                    break;
+                case "3":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid option, try again.");
+                    Main(null);
+                    break;
+            }
+            Console.WriteLine("Press any key to go back to the main menu...");
+            Console.ReadKey();
+            Main(null);
+        }
+    }
+}
